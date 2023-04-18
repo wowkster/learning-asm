96257a2f8743ed2f43cad8eec4456c24ac4886e0
Wowkster <49880655+wowkster@users.noreply.github.com>
2023-04-17 21:13:31 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Get the directory path passed as an argument
+
+if [ -z "$1" ]; then
+    echo "Please pass the directory path as an argument"
+    exit 1
+fi
+
+# Check to see if the directory exists 
+
+if [ ! -d "$1" ]; then
+    echo "Directory does not exist"
+    exit 1
+fi
+
+# Change to the directory
+
+cd "$1"
+
+# Assemble and link
+
+mkdir -p target
+
+nasm -f elf32 $1.asm -o ./target/$1.o
+ld -m elf_i386 -s -o ./target/$1 ./target/$1.o
+
+# Run if "run" is passed as an argument
+
+if [ "$2" = "run" ]; then
+    "./target/$1"
+fi
